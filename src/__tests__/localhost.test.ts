8eb00fd6e92886829564eb1f4ac3c32f1abe7b0b
Masato Ohira <ohira.ekzm@gmail.com>
2023-05-22 16:09:23 +0900

    fix

--- src/__tests__/localhost.test.ts
+++ src/__tests__/localhost.test.ts
@@ -3,5 +3,9 @@ import axios from 'axios'
 test('localhostが起動している', async () => {
   const url = 'http://localhost:3000'
   const response = await axios.get(url)
+
+  if (response.status !== 200) {
+    console.log('テストの実行前にlocalhostを起動してください')
+  }
   expect(response.status).toBe(200)
 })
