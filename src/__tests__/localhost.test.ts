a8483497a8aa285d93f9d2a34e466792b0381a3c
Masato Ohira <ohira.ekzm@gmail.com>
2023-05-22 16:24:45 +0900

    fix

--- src/__tests__/localhost.test.ts
+++ src/__tests__/localhost.test.ts
@@ -2,10 +2,11 @@ import axios from 'axios'
 
 test('localhostが起動している', async () => {
   const url = 'http://localhost:3000'
-  const response = await axios.get(url)
 
-  if (response.status !== 200) {
-    console.log('テストの実行前にlocalhostを起動してください')
+  try {
+    const response = await axios.get(url)
+    expect(response.status).toBe(200)
+  } catch (error) {
+    throw new Error('localhostの起動が確認できませんでした')
   }
-  expect(response.status).toBe(200)
 })
